c05e0c3380d0e743f1ff63ad2db1b1c8dd2829ca
karenanndonnachie <karenanndonnachie@gmail.com>
2020-10-14 14:04:22 +1100

    sorry, this should be better

--- only_playing_one_sound.js
+++ only_playing_one_sound.js
@@ -135,18 +135,19 @@ function draw() {
     
     //IMAGE PIXELATION+PIXEL SOUND
     if (img) {
+          notePattern=[];
     image(img,0,0,img.width,img.height);
         for (var x = 0; x < img.width; x+=pixSize){
     for (var y = 0; y < img.height; y+=pixSize){
      pix = get(x,y);
-     console.log('pix:'+pix);
+     //console.log('pix:'+pix);
      bright = brightness(pix);
-    console.log ('bright:'+bright);
+    //console.log ('bright:'+bright);
     fill(pix);
     strokeWeight(1);
     stroke(pix);
     rect(x+1,y+1,pixSize-1,pixSize-1);
-    notePattern=[];
+
     notePattern.push(int(pix));
     }
    }
@@ -177,6 +178,7 @@ function mousePressed() {
 
 //SOUND PLAYER BUTTON
 function togglePlaying(){
+  console.log(notePattern);
   // ensure audio is enabled
   userStartAudio();
   
