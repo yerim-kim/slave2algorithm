30e843c033daa361ec136925cbfb94cc773bf07b
yerim-kim <68723268+yerim-kim@users.noreply.github.com>
2020-10-20 18:00:40 +1100

    Update sound.js

--- week6/sound.js
+++ week6/sound.js
@@ -1,13 +1,37 @@
 var song; //
 var analyzer;
 function preload(){
-  song = loadSound
+  song = loadSound('Joseph Vincent & Jules Aurora - Rewrite The Stars.mp3');
 }
 function setup() {
-
+ createCanvas(windowWidth,windowHeight);
+ background(0);
+ fill(0,255,0);
+ //song.loop();
+ 
+ analyzer= new p5.Amplitude();
+ analyzer.setInput(song);
+ stroke(255);
 }
 
 
 function draw() {
+ background(0,50);
+ var volume=analyzer.getLevel();
+ volume=(volume*windowWidth)+60;
+ ellipse(windowWidth/2, windowHeight/2, volume,volume);
+}
 
+function mousePressed(){
+  if (song.isPlaying()) {
+    song.stop();
+    song.noloop();
+    fill(0,255,0);
+  } 
+  
+  else{
+  fill(255,0,0);
+  song.play();
+  song.loop();
+}
 }
